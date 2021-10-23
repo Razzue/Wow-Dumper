19f758ef3239ce0a453a23a7cf7deed8a352b05d
Xemnas-Vault <45306927+Razzue@users.noreply.github.com>
2021-10-23 10:31:16 -0600

    .net6 update and formatting
    Cleaned up the project and migrated from .Net framework 4.8 to .Net 6.
    
    Split the multiple patterns between expansion specific classes, all patters working as of October23, 2021 except for:
    - [Classic Era] Addon Base
    - [Classic Era] Keybind Base

---
+++ Wow-Scanner/Dumpers/ClassWriter.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace Wow_Scanner.Dumpers
+{
+    internal class ClassWriter
+    {
+        private static CodeCompileUnit OffsetUnit;
+        private static CodeNamespace OffsetNamespace;
+        private static CodeTypeDeclaration OffsetClass;
+
+        internal static bool SaveOffsetClass(Dictionary<string, IntPtr> Offsets)
+        {
+            try
+            {
+                OffsetUnit = new CodeCompileUnit();
+                OffsetNamespace = new CodeNamespace("Dumper.Offsets");
+
+                OffsetClass = new CodeTypeDeclaration("WowOffsets")
+                {
+                    IsClass = true,
+                    TypeAttributes = TypeAttributes.Public
+                };
+
+                foreach (var p in Offsets)
+                    OffsetClass.Members.Add(new CodeSnippetTypeMember($"        public const int {p.Key} = 0x{p.Value.ToInt64():X};"));
+
+                OffsetNamespace.Types.Add(OffsetClass);
+                OffsetUnit.Namespaces.Add(OffsetNamespace);
+                return SaveFile();
+            }
+            catch (Exception)
+            {
+                return false;
+            }    
+
+        }
+
+        private static bool SaveFile()
+        {
+            try
+            {
+                var Path = $@"{Environment.CurrentDirectory}\Offsets.cs";
+                var provider = CodeDomProvider.CreateProvider("CSharp");
+                var providerOptions = new CodeGeneratorOptions()
+                {
+                    BlankLinesBetweenMembers = true,
+                    BracingStyle = "C"
+                };
+
+                using var Writer = new StreamWriter(Path);
+                provider.GenerateCodeFromCompileUnit(OffsetUnit, Writer, providerOptions);
+
+                return File.Exists(Path);
+            }
+            catch (Exception )
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Wow-Scanner/Program.cs
@@ -0,0 +1,78 @@
+﻿using Wow_Scanner;
+using Wow_Scanner.Dumpers;
+
+Console.WriteLine("Dumper will attach to first active classic client.");
+Console.WriteLine("If no classic clients are open, it will attach to a retail client instead.");
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+
+var CanContinue = false;
+
+if (!WoW.FindWow())
+    Console.WriteLine("Could not find a classic, or retail client.");
+else
+{
+    if (!WoW.GetHandle())
+       Console.WriteLine("Could not get the client handle."); 
+    else
+    {
+        Console.WriteLine($"Loaded World of Warcraft {WoW.ClientVersion} {WoW.BuildVersion}");
+        CanContinue = true;
+    }
+}
+
+if (CanContinue)
+{
+    var Scanner = new Scan();
+    var expac = WoW.ClientVersion.Split('.').FirstOrDefault();
+
+    switch (expac)
+    {
+        case "Version 1":
+            if (VanillaMastery.LoadOffsets(Scanner))
+            {
+                foreach (var offset in VanillaMastery.Offsets)
+                    Console.WriteLine($"{offset.Key} -> 0x{offset.Value.ToInt64():X}");
+
+                if (ClassWriter.SaveOffsetClass(VanillaMastery.Offsets))
+                    Console.WriteLine("Offsets saved to class file.");
+            }
+            else Console.WriteLine("Could not load offsets?");
+            break;
+
+        case "Version 2":
+            if (BurningCrusade.LoadOffsets(Scanner))
+            {
+                foreach (var offset in BurningCrusade.Offsets)
+                    Console.WriteLine($"{offset.Key} -> 0x{offset.Value.ToInt64():X}");
+
+                if (ClassWriter.SaveOffsetClass(BurningCrusade.Offsets))
+                    Console.WriteLine("Offsets saved to class file.");
+            }
+            else Console.WriteLine("Could not load offsets?");
+            break;
+
+        case "Version 9":
+            if (Shadowlands.LoadOffsets(Scanner))
+            {
+                foreach (var offset in Shadowlands.Offsets)
+                    Console.WriteLine($"{offset.Key} -> 0x{offset.Value.ToInt64():X}");
+
+                if (ClassWriter.SaveOffsetClass(Shadowlands.Offsets))
+                    Console.WriteLine("Offsets saved to class file.");
+            }
+            else Console.WriteLine("Could not load offsets?"); 
+                break;
+
+        default:
+            Console.WriteLine("Could not load the client expansion.");
+            break;
+    }
+
+    
+}
+
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
