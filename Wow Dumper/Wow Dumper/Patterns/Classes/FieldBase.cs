8ca248215df8b5ebb70b6b1482183e1efbd85e57
Xemnas-Vault <45306927+Razzue@users.noreply.github.com>
2022-01-09 15:56:21 -0700

    Removed version one and other junk

--- Wow Dumper/Wow Dumper/Patterns/Classes/FieldBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wow_Dumper.Patterns
-{
-    public class FieldBase
-    {
-        public FieldType Type { get; set; } = FieldType.Null;
-        public string? Name { get; set; } = null;
-        public int Position { get; set; } = 0;
-    }
-}
--- Wow Dumper/Wow Dumper/Patterns/Classes/LevelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wow_Dumper.Patterns
-{
-    public class LevelBase
-    {
-        public string? Name { get; set; } = null;
-        public int Position { get; set; } = 0;
-        public int Additional { get; set; } = 0;
-    }
-}
